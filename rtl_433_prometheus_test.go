3fbd43af902112d99cf13389b89889e64a85a4b8
Alexandre Macabies <web+oss@zopieux.com>
2020-08-29 00:05:40 +0200

    Add missing location="" in expected metrics in tests.

--- rtl_433_prometheus_test.go
+++ rtl_433_prometheus_test.go
@@ -47,10 +47,10 @@ func TestParsingToMetrics(t *testing.T) {
 	wantTemperature := `
 		# HELP rtl_433_temperature_celsius Temperature in Celsius
 		# TYPE rtl_433_temperature_celsius gauge
-		rtl_433_temperature_celsius{channel="1",id="94",model="Nexus-TH"} 22.6
-		rtl_433_temperature_celsius{channel="2",id="184",model="Nexus-TH"} 21.7
-		rtl_433_temperature_celsius{channel="3",id="55",model="Ecowitt-WH53"} 18
-		rtl_433_temperature_celsius{channel="A",id="7997",model="Acurite-Tower"} 12.6
+		rtl_433_temperature_celsius{channel="1",id="94",location="",model="Nexus-TH"} 22.6
+		rtl_433_temperature_celsius{channel="2",id="184",location="",model="Nexus-TH"} 21.7
+		rtl_433_temperature_celsius{channel="3",id="55",location="",model="Ecowitt-WH53"} 18
+		rtl_433_temperature_celsius{channel="A",id="7997",location="",model="Acurite-Tower"} 12.6
 	`
 
 	if err := testutil.CollectAndCompare(temperature, strings.NewReader(wantTemperature), "rtl_433_temperature_celsius"); err != nil {
@@ -60,9 +60,9 @@ func TestParsingToMetrics(t *testing.T) {
 	wantHumidity := `
 		# HELP rtl_433_humidity Relative Humidity (0-1.0)
 		# TYPE rtl_433_humidity gauge
-		rtl_433_humidity{channel="1",id="94",model="Nexus-TH"} 0.53
-		rtl_433_humidity{channel="2",id="184",model="Nexus-TH"} 0.55
-		rtl_433_humidity{channel="A",id="7997",model="Acurite-Tower"} 0.91
+		rtl_433_humidity{channel="1",id="94",location="",model="Nexus-TH"} 0.53
+		rtl_433_humidity{channel="2",id="184",location="",model="Nexus-TH"} 0.55
+		rtl_433_humidity{channel="A",id="7997",location="",model="Acurite-Tower"} 0.91
 	`
 	if err := testutil.CollectAndCompare(humidity, strings.NewReader(wantHumidity), "rtl_433_humidity"); err != nil {
 		t.Errorf("unexpected collecting result:\n%s", err)
@@ -71,10 +71,10 @@ func TestParsingToMetrics(t *testing.T) {
 	wantPacketsReceived := `
 		# HELP rtl_433_packets_received Packets (temperature messages) received.
 		# TYPE rtl_433_packets_received counter
-		rtl_433_packets_received{channel="1",id="94",model="Nexus-TH"} 1
-		rtl_433_packets_received{channel="2",id="184",model="Nexus-TH"} 1
-		rtl_433_packets_received{channel="3",id="55",model="Ecowitt-WH53"} 1
-		rtl_433_packets_received{channel="A",id="7997",model="Acurite-Tower"} 1
+		rtl_433_packets_received{channel="1",id="94",location="",model="Nexus-TH"} 1
+		rtl_433_packets_received{channel="2",id="184",location="",model="Nexus-TH"} 1
+		rtl_433_packets_received{channel="3",id="55",location="",model="Ecowitt-WH53"} 1
+		rtl_433_packets_received{channel="A",id="7997",location="",model="Acurite-Tower"} 1
 	`
 	if err := testutil.CollectAndCompare(packetsReceived, strings.NewReader(wantPacketsReceived), "rtl_433_packets_received"); err != nil {
 		t.Errorf("unexpected collecting result:\n%s", err)
@@ -82,9 +82,9 @@ func TestParsingToMetrics(t *testing.T) {
 	wantBattery := `
 		# HELP rtl_433_battery Battery high (1) or low (0).
 		# TYPE rtl_433_battery gauge
-		rtl_433_battery{channel="1",id="94",model="Nexus-TH"} 1
-		rtl_433_battery{channel="2",id="184",model="Nexus-TH"} 1
-		rtl_433_battery{channel="A",id="7997",model="Acurite-Tower"} 0
+		rtl_433_battery{channel="1",id="94",location="",model="Nexus-TH"} 1
+		rtl_433_battery{channel="2",id="184",location="",model="Nexus-TH"} 1
+		rtl_433_battery{channel="A",id="7997",location="",model="Acurite-Tower"} 0
 	`
 	if err := testutil.CollectAndCompare(battery, strings.NewReader(wantBattery), "rtl_433_battery"); err != nil {
 		t.Errorf("unexpected collecting result:\n%s", err)
